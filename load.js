8e3d3f313011a062770168df8bcb60dfcc215380
sturdystablestalwart <laosen9@gmail.com>
2023-07-19 22:08:22 +0300

    fixed eternal polling bug, fixed timeout before the first poll

--- load.js
+++ load.js
@@ -1,51 +1,45 @@
-// Button listener and transformation section.
+// Button listener and transformation section
+let timer;
 $('#200001').click(function() {
     let self = this;
     handleButtonClick(self);
 });
-function handleButtonClick( el ) {
+function handleButtonClick(el) {
     let sourceUrl = 'https://gist.githubusercontent.com/sturdystablestalwart/' +
-        'c01e34d434af129e6f7e6ddfbf097926/raw/Data_for_the_TT.json'
-    let urlFromInput = document.getElementById("100001").value
-    let isActive = true
-    if (isValidUrl(urlFromInput)){sourceUrl = urlFromInput}
+        'c01e34d434af129e6f7e6ddfbf097926/raw/Data_for_the_TT.json';
+    let urlFromInput = document.getElementById("100001").value;
 
     if ( el.value === "Read Json and watch the source" ){
-        el.value = "Stop polling"
+        el.value = "Stop polling";
         if (isValidUrl(urlFromInput)){
             sourceUrl = urlFromInput;
         }
-        loadJson(isActive, sourceUrl);
+        loadJson(sourceUrl);
+        timer = setInterval(loadJson, 5000, sourceUrl);
     }
     else
+    {
         el.value = "Read Json and watch the source";
         isActive = false;
-        loadJson(isActive, sourceUrl);
+        clearInterval(timer);
+    }
 }
 
 // Loading section.
-function loadJson(isActive, sourceUrl)
+function loadJson(sourceUrl)
 {
-    if (isActive)
-    {
-        window.setTimeout(function () {
-            $.ajax({
-                url: sourceUrl,
-                dataType: 'json',
-                success: function (data) {
-                    document.getElementById("000001").innerHTML = "The name field is " + data.name;
-                    loadJson(isActive, sourceUrl);
-                },
-                error: function (error) {
-                    document.getElementById("000001").innerHTML =
-                        "Error occurred while trying to load Json from the server " + error
-                    loadJson(isActive, sourceUrl);
-                }});
-        }, 30000);
-    }
+    $.ajax({
+        url: sourceUrl,
+        dataType: 'json',
+        success: function (data) {
+            document.getElementById("000001").innerHTML = "The name field is " + data.name;
+        },
+        error: function (error) {
+            document.getElementById("000001").innerHTML =
+                "Error occurred while trying to load Json from the server " + error
+        }
+    });
 }
-//TODO create a solution for initial timeout
-//TODO create a solution for stopping the polling
 
 // Url validator.
 function isValidUrl(string) {
