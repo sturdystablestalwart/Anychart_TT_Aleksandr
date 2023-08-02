ddb9e70c52267d6d5abeb403a4b534500df33a53
sturdystablestalwart <laosen9@gmail.com>
2023-08-02 22:39:51 +0300

    Fixed a bug with chart flowing out of the canvas area and added ticks for the X axes, started writing a logick for the Y Axes

--- chart.js
+++ chart.js
@@ -10,8 +10,8 @@ function startCharting(pointsMeasurementsBundle, pointsData){
 
     const canvasParameters = findDimentions(svg);
     drawMainLine(pointsData, canvasParameters, pointsMeasurementsBundle, svg);
-    drawXAxes(canvasParameters, svg);
-    drawYAxes(canvasParameters, svg); // TODO solve bug with the mainLine leaving the canvas
+    drawYAxes(canvasParameters, svg);
+    drawXAxes(canvasParameters,pointsMeasurementsBundle, svg);
 }
 
 function findDimentions(svg){
@@ -21,30 +21,34 @@ function findDimentions(svg){
     const canvasHeight = svgHeight * 0.9;
     const canvasWidth = svgWidth * 0.9;
     const minX = (svgWidth - canvasWidth) / 2;
+    const maxX = minX + canvasWidth;
     const minY = (svgHeight - canvasHeight) / 2;
+    const maxY = minY + canvasHeight;
 
-    const canvasParameters = {canvasHeight, canvasWidth, minX, minY};
+    const canvasParameters = {canvasHeight, canvasWidth, maxX, maxY, minX, minY};
 
     return canvasParameters;
 }
 
 function drawMainLine(pointsData, canvasParameters, pointsMeasurementsBundle, svg){
 
-    const {canvasHeight, canvasWidth, minX, minY} = canvasParameters;
+    const {canvasHeight, canvasWidth, maxX, maxY, minX, minY} = canvasParameters;
 
-    let sectionLength = canvasWidth / (pointsMeasurementsBundle[0] - 1); // длина отрезка линии графика между точками
+    let sectionLength = canvasWidth / (pointsMeasurementsBundle[0]); // расстояние между точками
     
-    let x = minX;
+    let x = minX + sectionLength / 2;
     let y = minY;
     let z = canvasHeight / (pointsMeasurementsBundle[1] - pointsMeasurementsBundle[2]); // приближение
         z = (z == Infinity) ? 1 : z;
 
-    let yCentrationShift = minY;
-    let ySubZeroCompensation = pointsMeasurementsBundle[2] * z;
+    let ySubZeroCompensation = 0;
+    if (pointsMeasurementsBundle[2]<0) {
+        ySubZeroCompensation = pointsMeasurementsBundle[2] * z - minY;
+    }
 
     let d = 'M';
-    pointsData.forEach(element => {        
-        y = canvasHeight - element.value * z + ySubZeroCompensation + yCentrationShift;
+    pointsData.forEach( (element) => {        
+        y = maxY - element.value * z + ySubZeroCompensation  + minY;
         d += ' ' + x + ' ' + y;
         x += sectionLength;
     });
@@ -57,10 +61,11 @@ function drawMainLine(pointsData, canvasParameters, pointsMeasurementsBundle, sv
     svg.appendChild(pathLine);
 }
 
-function drawXAxes(canvasParameters, svg){
-    const {canvasHeight, minX, minY} = canvasParameters;
+/* Axes drawing section */
+function drawYAxes(canvasParameters, svg){
+    const {maxY, minX, minY} = canvasParameters;
 
-    let d = 'M ' + minX + ' ' + canvasHeight + ' ' + minX + ' ' + minY;
+    const d = 'M ' + minX + ' ' + maxY + ' ' + minX + ' ' + minY;
 
     const xAxesLine = document.createElementNS("http://www.w3.org/2000/svg", "path");
           xAxesLine.setAttributeNS(null, "d", d);
@@ -69,14 +74,24 @@ function drawXAxes(canvasParameters, svg){
           xAxesLine.setAttributeNS(null, "stroke-width", 1);
     svg.appendChild(xAxesLine);
 
-    drawXAxesArrowPoint(minX, minY, svg);
+    drawYAxesArrowPoint(minX, minY, svg);
+    // drawYTicks(canvasParameters, pointsMeasurementsBundle, svg);
 }
 
-function drawXAxesArrowPoint(minX, minY, svg){    
+function drawYTicks(canvasParameters, pointsMeasurementsBundle, svg){
+    const {canvasHeight, canvasWidth, maxX, maxY, minX, minY} = canvasParameters;
+    let z = canvasHeight / (pointsMeasurementsBundle[1] - pointsMeasurementsBundle[2]); // приближение
+        z = (z == Infinity) ? 1 : z;
+    
+    
+    
+}
+
+function drawYAxesArrowPoint(minX, minY, svg){    
     const leftArrowWingPointCoordinates = ' ' + (minX - 3) + ' ' + (minY + 5);
     const rightArrowWingPointCoordinates = ' ' + (minX + 3) + ' ' + (minY + 5);
 
-    d = 'M ' + minX + ' ' + minY + leftArrowWingPointCoordinates + rightArrowWingPointCoordinates + " Z";
+    const d = 'M ' + minX + ' ' + minY + leftArrowWingPointCoordinates + rightArrowWingPointCoordinates + " Z";
 
     const xAxesLineArrow = document.createElementNS("http://www.w3.org/2000/svg", "path");
           xAxesLineArrow.setAttributeNS(null, "d", d);
@@ -86,31 +101,59 @@ function drawXAxesArrowPoint(minX, minY, svg){
     svg.appendChild(xAxesLineArrow);
 }
 
-function drawYAxes(canvasParameters, svg){
-    const {canvasHeight, canvasWidth, minX} = canvasParameters;
-
-    let d = 'M ' + minX + ' ' + canvasHeight + ' ' + canvasWidth + ' ' + canvasHeight;
-
+function drawXAxes(canvasParameters, pointsMeasurementsBundle, svg){
+    const {maxX, maxY, minX} = canvasParameters;
+    
+    const d = 'M ' + minX + ' ' + maxY + ' ' + maxX + ' ' + maxY;
+    
     const xAxesLine = document.createElementNS("http://www.w3.org/2000/svg", "path");
-          xAxesLine.setAttributeNS(null, "d", d);
-          xAxesLine.setAttributeNS(null, "fill", "none");
-          xAxesLine.setAttributeNS(null, "stroke", '#696969');
-          xAxesLine.setAttributeNS(null, "stroke-width", 1);
+    xAxesLine.setAttributeNS(null, "d", d);
+    xAxesLine.setAttributeNS(null, "fill", "none");
+    xAxesLine.setAttributeNS(null, "stroke", '#696969');
+    xAxesLine.setAttributeNS(null, "stroke-width", 1);
     svg.appendChild(xAxesLine);
 
-    drawYAxesArrowPoint(canvasWidth, canvasHeight, svg);
+    drawXAxesArrowPoint(maxX, maxY, svg);
+    drawXTicks(canvasParameters, pointsMeasurementsBundle, svg);
 }
 
-function drawYAxesArrowPoint(canvasWidth, canvasHeight, svg){    
-    const heigherArrowWingPointCoordinates = ' ' + (canvasWidth - 5) + ' ' + (canvasHeight - 3);
-    const lowerArrowWingPointCoordinates = ' ' + (canvasWidth - 5) + ' ' + (canvasHeight + 3);
+function drawXAxesArrowPoint(maxX, maxY, svg){    
+    const heigherArrowWingPointCoordinates = ' ' + (maxX - 5) + ' ' + (maxY - 3);
+    const lowerArrowWingPointCoordinates = ' ' + (maxX - 5) + ' ' + (maxY + 3);
 
-    d = 'M ' + canvasWidth + ' ' + canvasHeight + heigherArrowWingPointCoordinates + lowerArrowWingPointCoordinates + " Z";
+    const d = 'M ' + maxX + ' ' + maxY + heigherArrowWingPointCoordinates + lowerArrowWingPointCoordinates + " Z";
 
     const xAxesLineArrow = document.createElementNS("http://www.w3.org/2000/svg", "path");
           xAxesLineArrow.setAttributeNS(null, "d", d);
           xAxesLineArrow.setAttributeNS(null, "fill", "#696969");
           xAxesLineArrow.setAttributeNS(null, "stroke", '#696969');
           xAxesLineArrow.setAttributeNS(null, "stroke-width", 1);
-    svg.appendChild(xAxesLineArrow);
+          svg.appendChild(xAxesLineArrow);
+}
+
+function drawXTicks(canvasParameters, pointsMeasurementsBundle, svg){
+
+    const {canvasWidth, maxY, minX} = canvasParameters;
+
+    let sectionLength = canvasWidth / (pointsMeasurementsBundle[0]); // расстояние между точками
+    
+    let x = minX + sectionLength / 2;
+
+    let d = 'M';
+    for (let index = pointsMeasurementsBundle[0]; index != 0; index--) {
+        let y = maxY;
+        d += ' ' + x + ' ' + y + ' V';
+        y += 3;
+        d += ' ' + y + ' M';
+        x += sectionLength;
+    }
+
+    d = d.slice(0, -2);
+
+    const xAxesLineTicks = document.createElementNS("http://www.w3.org/2000/svg", "path");
+          xAxesLineTicks.setAttributeNS(null, "d", d);
+          xAxesLineTicks.setAttributeNS(null, "fill", "#696969");
+          xAxesLineTicks.setAttributeNS(null, "stroke", '#696969');
+          xAxesLineTicks.setAttributeNS(null, "stroke-width", 1);
+    svg.appendChild(xAxesLineTicks);    
 }
